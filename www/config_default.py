3475ebe145a1d5e73977d2a81f0bc29e954d7fe5
y <cnbjyez@gmail.com>
2018-05-15 15:05:06 +0800

    add configs

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'y'
+
+configs = {
+    'db' : {
+        'host' : '127.0.0.1',
+        'port' : 3306,
+        'user' : 'root',
+        'password' : '',
+        'database' : 'awesome'
+    },
+    'session' : {
+        'secret': 'AwEsOmE'
+    }
+}
