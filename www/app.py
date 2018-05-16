f2357974e5311890920b4119331df1ee9a23ab51
y <cnbjyez@gmail.com>
2018-05-16 11:14:43 +0800

    signin,signout,static files

--- www/app.py
+++ www/app.py
@@ -19,6 +19,7 @@ from jinja2 import Environment, FileSystemLoader
 
 import orm
 from coroweb import add_routes, add_static
+from handlers import COOKIE_NAME, cookie2user
 
 
 def init_jinja2(app, **kw):
@@ -48,6 +49,22 @@ async def logger_factory(app, handler):
         logging.info('Request: %s %s' % (request.method, request.path))
         return (await handler(request))
     return logger
+
+
+async def auth_factory(app, handler):
+    async def auth(request):
+        logging.info('check user: %s, %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return await handler(request)
+    return auth
 
 
 async def data_factory(app, handler):
@@ -86,6 +103,7 @@ async def response_factory(app, handler):
                 resp.content_type = 'application/json;charset=utf-8'
                 return resp
             else:
+                r['__user__'] = request.__user__
                 resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
                 resp.content_type = 'text/html;charset=utf-8'
                 return resp
@@ -119,7 +137,7 @@ def datetime_filter(t):
 async def init(loop):
     await orm.create_pool(loop= loop, host='127.0.0.1', port=3306, user='root', password='', db= 'awesome')
     app = web.Application(loop=loop, middlewares=[
-        logger_factory, response_factory
+        logger_factory, auth_factory, response_factory
     ])
     init_jinja2(app, filters=dict(datetime=datetime_filter))
     add_routes(app, 'handlers')
